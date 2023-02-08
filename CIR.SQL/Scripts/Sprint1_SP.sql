24cee5109f3205cf1a1955bfdd0287771b4a1060
mittalvyas1 <mittal.vyas@tatvasoft.com>
2023-02-08 10:57:13 +0530

    DB related changes

---
+++ CIR.SQL/Scripts/Sprint1_SP.sql
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIR.SQL.Scripts
+{
+    internal class Sprint1_SP
+    {
+    }
+}
