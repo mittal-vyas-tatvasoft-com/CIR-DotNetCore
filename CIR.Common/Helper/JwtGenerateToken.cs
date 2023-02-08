871e6eae9dfd24497563b63c47d33657e1523b6e
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2023-02-08 14:59:33 +0530

    Admin Login DBcontext and structure

---
+++ CIR.Common/Helper/JwtGenerateToken.cs
@@ -0,0 +1,15 @@
+﻿using CIR.Common.CommonModels;
+using CIR.Core.Entities.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CIR.Common.Helper
+{
+    public class JwtGenerateToken
+    {
+        
+    }
+}
---
+++ CIR.Core/Interfaces/ILoginRepository.cs
@@ -0,0 +1,15 @@
+﻿using CIR.Core.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIR.Core.Interfaces
+{
+ public interface ILoginRepository
+    {
+       
+    }
+}
