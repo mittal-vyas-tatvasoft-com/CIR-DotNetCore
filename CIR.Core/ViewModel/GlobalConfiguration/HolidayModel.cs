fd7bc7888677185769a59462081683448d740fe5
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-02-09 09:32:10 +0530

    Holiday Model changed

--- CIR.Core/ViewModel/GlobalConfiguration/HolidayModel.cs
+++ CIR.Core/ViewModel/GlobalConfiguration/HolidayModel.cs
@@ -4,14 +4,15 @@
     {
         public int TotalCount { get; set; }
         public List<HolidayViewModel> Holidays { get; set; }
-        public class HolidayViewModel
-        {
-            public long Id { get; set; }
-            public long CountryId { get; set; }
-            public DateTime Date { get; set; }
-            public string Description { get; set; }
-            public string CountryCode { get; set; }
-            public string CountryName { get; set; }
-        }
+    }
+
+    public class HolidayViewModel
+    {
+        public long Id { get; set; }
+        public long CountryId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
     }
 }
