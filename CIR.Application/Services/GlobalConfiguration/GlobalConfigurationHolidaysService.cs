bde83c56de4b9ff81ae6b0d0f53ee6e2fbf45430
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-02-13 15:28:27 +0530

    Holidays-APIs

--- CIR.Application/Services/GlobalConfiguration/GlobalConfigurationHolidaysService.cs
+++ CIR.Application/Services/GlobalConfiguration/GlobalConfigurationHolidaysService.cs
@@ -1,9 +1,36 @@
-﻿using CIR.Core.Interfaces.GlobalConfiguration;
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using CIR.Core.Interfaces.GlobalConfiguration;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CIR.Application.Services.GlobalConfiguration
 {
     public class GlobalConfigurationHolidaysService : IGlobalConfigurationHolidaysService
     {
+        private readonly IGlobalConfigurationHolidaysRepository globalConfigurationHolidaysRepository;
 
+        public GlobalConfigurationHolidaysService(IGlobalConfigurationHolidaysRepository globalConfigurationHolidaysRepository)
+        {
+            this.globalConfigurationHolidaysRepository = globalConfigurationHolidaysRepository;
+        }
+
+        public async Task<IActionResult> CreateOrUpdateGlobalConfigurationHoliday(Holidays holiday)
+        {
+            return await globalConfigurationHolidaysRepository.CreateOrUpdateGlobalConfigurationHoliday(holiday);
+        }
+
+        public async Task<IActionResult> DeleteGlobalConfigurationHoliday(long id)
+        {
+            return await globalConfigurationHolidaysRepository.DeleteGlobalConfigurationHoliday(id);
+        }
+
+        public async Task<IActionResult> GetAllGlobalConfigurationHolidays(int displayLength, int displayStart, string? sortCol, string? search, int? countryCodeId, int? countryNameId, bool? sortAscending = true)
+        {
+            return await globalConfigurationHolidaysRepository.GetAllGlobalConfigurationHolidays(displayLength, displayStart, sortCol, search, countryCodeId, countryNameId, sortAscending);
+        }
+
+        public async Task<IActionResult> GetGlobalConfigurationHolidayById(long id)
+        {
+            return await globalConfigurationHolidaysRepository.GetGlobalConfigurationHolidayById(id);
+        }
     }
 }
--- CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationHolidaysService.cs
+++ CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationHolidaysService.cs
@@ -1,7 +1,13 @@
-﻿namespace CIR.Core.Interfaces.GlobalConfiguration
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIR.Core.Interfaces.GlobalConfiguration
 {
     public interface IGlobalConfigurationHolidaysService
     {
-
+        public Task<IActionResult> GetGlobalConfigurationHolidayById(long id);
+        public Task<IActionResult> GetAllGlobalConfigurationHolidays(int displayLength, int displayStart, string? sortCol, string? search, int? countryCodeId, int? countryNameId, bool? sortAscending = true);
+        public Task<IActionResult> CreateOrUpdateGlobalConfigurationHoliday(Holidays holiday);
+        public Task<IActionResult> DeleteGlobalConfigurationHoliday(long id);
     }
 }
