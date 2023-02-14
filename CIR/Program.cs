a7c8888740234dd11b248ff412afcf9b91db1b28
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-02-14 11:11:03 +0530

    Added namespace

--- CIR/Program.cs
+++ CIR/Program.cs
@@ -1,3 +1,20 @@
+using CIR.Application.Services;
+using CIR.Application.Services.Common;
+using CIR.Application.Services.GlobalConfiguration;
+using CIR.Common.CommonModels;
+using CIR.Common.Data;
+using CIR.Common.EmailGeneration;
+using CIR.Common.Helper;
+using CIR.Core.Interfaces;
+using CIR.Core.Interfaces.Common;
+using CIR.Core.Interfaces.GlobalConfiguration;
+using CIR.Data.Data;
+using CIR.Data.Data.Common;
+using CIR.Data.Data.GlobalConfiguration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
