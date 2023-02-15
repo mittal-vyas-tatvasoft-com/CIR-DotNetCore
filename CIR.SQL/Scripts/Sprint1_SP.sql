217925f46cc0a8bced939adcd641a22047aac6e3
mihir1678 <mihir.rathod@tatvamail.com>
2023-02-15 10:00:17 +0530

    conflict resolved

--- CIR.SQL/Scripts/Sprint1_SP.sql
+++ CIR.SQL/Scripts/Sprint1_SP.sql
@@ -482,3 +482,74 @@ AS
 BEGIN
 	select * from CountryCodes
 END
+
+-- GlobalCutOffTimes SP start
+
+-- spGetGlobalConfigurationCutOffTimesByCountryId start
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[spGetGlobalConfigurationCutOffTimesByCountryId]( @CountryId bigint)
+
+AS
+BEGIN
+	SELECT TOP 1 * FROM GlobalConfigurationCutOffTimes where CountryId = @CountryId
+END
+GO
+-- spGetGlobalConfigurationCutOffTimesByCountryId end
+
+-- spCreateOrUpdateGlobalConfigurationCutOffTimes start
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[spCreateOrUpdateGlobalConfigurationCutOffTimes]  
+(  
+ @Id bigint,  
+ @CountryId bigint,  
+ @CutOffTime time(7),  
+ @CutOffDay smallint  
+)  
+  
+AS  
+BEGIN  
+ Declare @Result bit = 0;  
+  
+ IF(@Id = 0 and exists(select 1 from CountryCodes where Id = @CountryId) and not exists(select 1 from GlobalConfigurationCutOffTimes where CountryId = @CountryId))  
+ BEGIN  
+   INSERT INTO GlobalConfigurationCutOffTimes(CountryId, CutOffTime, CutOffDay)  
+   VALUES (@CountryId, @CutOffTime, @CutOffDay)  
+  
+   SET @Result = 1;  
+  
+ END  
+ ELSE  
+  IF(exists(select 1 from CountryCodes where Id = @CountryId) and exists(select 1 from GlobalConfigurationCutOffTimes where CountryId = @CountryId))  
+   BEGIN  
+    UPDATE GlobalConfigurationCutOffTimes SET  
+    CountryId = @CountryId,  
+    CutOffTime = @CutOffTime,  
+    CutOffDay = @CutOffDay  
+    WHERE Id = @Id  
+
+	Update Portal2GlobalConfigurationCutOffTimes 
+	Set CutOffTimeOverride = @CutOffTime,  
+    CutOffDayOverride = @CutOffDay
+	Where GlobalConfigurationCutOffTimeId = @Id
+
+	SET @Result = 1;
+  
+   END  
+  
+ SELECT @Result  
+END 
+GO
+-- spCreateOrUpdateGlobalConfigurationCutOffTimes end
+-- GlobalCutOffTimes SP end
