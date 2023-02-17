5a144bd3c82961eedba0f457fdcc90abf1a4ada0
ritugohel1750 <ritu.gohel@tatvasoft.com>
2023-02-17 12:54:28 +0530

    Global configuration message development

--- CIR.Application/Services/GlobalConfiguration/GlobalConfigurationMessagesService.cs
+++ CIR.Application/Services/GlobalConfiguration/GlobalConfigurationMessagesService.cs
@@ -1,4 +1,6 @@
-﻿using CIR.Core.Interfaces.GlobalConfiguration;
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using CIR.Core.Interfaces.GlobalConfiguration;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,14 @@ namespace CIR.Application.Services.GlobalConfiguration
         public GlobalConfigurationMessagesService(IGlobalConfigurationMessagesRepository _globalConfigurationMessagesRepository)
         {
             globalConfigurationMessagesRepository = _globalConfigurationMessagesRepository;
+        }
+        public async Task<IActionResult> GetGlobalConfigurationMessagesList(int cultureId)
+        {
+            return await globalConfigurationMessagesRepository.GetGlobalConfigurationMessagesList(cultureId);
+        }
+        public async Task<IActionResult> CreateOrUpdateGlobalConfigurationMessages(List<GlobalConfigurationMessage> globalConfigurationMessages)
+        {
+            return await globalConfigurationMessagesRepository.CreateOrUpdateGlobalConfigurationMessages(globalConfigurationMessages);
         }
     }
 }
--- CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationMessagesService.cs
+++ CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationMessagesService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace CIR.Core.Interfaces.GlobalConfiguration
 {
     public interface IGlobalConfigurationMessagesService
     {
+        Task<IActionResult> GetGlobalConfigurationMessagesList(int cultureId);
+        Task<IActionResult> CreateOrUpdateGlobalConfigurationMessages(List<GlobalConfigurationMessage> globalConfigurationMessages);
     }
 }
