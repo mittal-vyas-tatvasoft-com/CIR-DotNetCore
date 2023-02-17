3b39a49e2c148f44885a29816d6630a3f5432291
Visvarajsinh <visvaraj.rathod@tatvamail.com>
2023-02-17 11:31:58 +0530

    created add new currency api method

--- CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationCurrenciesRepository.cs
+++ CIR.Core/Interfaces/GlobalConfiguration/IGlobalConfigurationCurrenciesRepository.cs
@@ -1,4 +1,5 @@
-﻿using CIR.Core.ViewModel.GlobalConfiguration;
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using CIR.Core.ViewModel.GlobalConfiguration;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CIR.Core.Interfaces.GlobalConfiguration
@@ -7,5 +8,7 @@ namespace CIR.Core.Interfaces.GlobalConfiguration
     {
         Task<IActionResult> GetGlobalConfigurationCurrenciesCountryWise(int countryId);
         Task<IActionResult> CreateOrUpdateGlobalConfigurationCurrencies(List<GlobalConfigurationCurrencyModel> globalConfigurationCurrencyModels);
+        Task<Boolean> CurrencyExists(string codeName);
+        Task<IActionResult> AddNewCurrency(Currency currency);
     }
 }
