7bb175cde642db2d891a06ad3d5d17020d208e73
ChiragMajethiyaTatvasoft <chirag.majethiya@tatvasoft.com>
2023-02-22 11:40:44 +0530

    merged with main

--- CIR.Common/Data/CIRDbContext.cs
+++ CIR.Common/Data/CIRDbContext.cs
@@ -1,4 +1,8 @@
-﻿namespace CIR.Common.Data
+﻿using CIR.Core.Entities.GlobalConfiguration;
+using CIR.Core.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIR.Common.Data
 {
 	public class CIRDbContext : DbContext
 	{
@@ -19,69 +23,48 @@
 			set;
 		}
 		public DbSet<Holidays> Holidays { get; set; }
-		public class CIRDbContext : DbContext
+
+		public DbSet<GlobalConfigurationCutOffTime> GlobalConfigurationCutOffTimes { get; set; }
+
+		public DbSet<RoleGrouping2Permission> RoleGrouping2Permissions
 		{
-			public CIRDbContext(DbContextOptions<CIRDbContext> options) : base(options) { }
-			public DbSet<Roles> Roles
-			{
-				get;
-				set;
-			}
-			public DbSet<RoleGrouping> RolesGroupings
-			{
-				get;
-				set;
-			}
-			public DbSet<RoleGrouping2Culture> RoleGrouping2Cultures
-			{
-				get;
-				set;
-			}
-			public DbSet<GlobalConfigurationCutOffTime> GlobalConfigurationCutOffTimes { get; set; }
-			public DbSet<Holidays> Holidays { get; set; }
-
-
-			public DbSet<RoleGrouping2Permission> RoleGrouping2Permissions
-			{
-				get;
-				set;
-			}
-			public DbSet<RoleGrouping2SubSite> RoleGrouping2SubSites
-			{
-				get;
-				set;
-			}
-			protected override void OnModelCreating(ModelBuilder modelBuilder)
-			{
-				modelBuilder.Entity<RoleGrouping2SubSite>().HasKey(x => new { x.RoleGroupingId, x.SubSiteId });
-				modelBuilder.Entity<RoleGrouping2Permission>().HasKey(x => new { x.RoleGroupingId, x.PermissionEnumId });
-				modelBuilder.Entity<RoleGrouping2Culture>().HasKey(x => new { x.RoleGroupingId, x.CultureLcid });
-			}
-			public DbSet<Currency> Currencies
-			{
-				get;
-				set;
-			}
-			public DbSet<CountryCode> CountryCodes
-			{
-				get;
-				set;
-			}
-			public DbSet<GlobalConfigurationCurrency> GlobalConfigurationCurrencies
-			{
-				get;
-				set;
-			}
-
-			public virtual DbSet<GlobalConfigurationEmails> GlobalConfigurationEmails { get; set; }
-
-			public DbSet<User> Users
-			{
-				get;
-				set;
-			}
-			public DbSet<GlobalConfigurationMessage> GlobalConfigurationMessages { get; set; }
+			get;
+			set;
 		}
+		public DbSet<RoleGrouping2SubSite> RoleGrouping2SubSites
+		{
+			get;
+			set;
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<RoleGrouping2SubSite>().HasKey(x => new { x.RoleGroupingId, x.SubSiteId });
+			modelBuilder.Entity<RoleGrouping2Permission>().HasKey(x => new { x.RoleGroupingId, x.PermissionEnumId });
+			modelBuilder.Entity<RoleGrouping2Culture>().HasKey(x => new { x.RoleGroupingId, x.CultureLcid });
+		}
+		public DbSet<Currency> Currencies
+		{
+			get;
+			set;
+		}
+		public DbSet<CountryCode> CountryCodes
+		{
+			get;
+			set;
+		}
+		public DbSet<GlobalConfigurationCurrency> GlobalConfigurationCurrencies
+		{
+			get;
+			set;
+		}
+
+		public virtual DbSet<GlobalConfigurationEmails> GlobalConfigurationEmails { get; set; }
+
+		public DbSet<User> Users
+		{
+			get;
+			set;
+		}
+		public DbSet<GlobalConfigurationMessage> GlobalConfigurationMessages { get; set; }
 	}
-
-
+}
