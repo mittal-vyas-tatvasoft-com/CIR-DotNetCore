9696689c6972ee7fa8f434a0de5cb27ebe68d3dc
mihir1678 <mihir.rathod@tatvamail.com>
2023-02-27 10:06:13 +0530

    GlobalConfiguration-Weekends-Folder Structure

---
+++ CIR/Controllers/GlobalConfiguration/GlobalConfigurationWeekendController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIR.Controllers.GlobalConfiguration
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GlobalConfigurationWeekendController : ControllerBase
+    {
+    }
+}
